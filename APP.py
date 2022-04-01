1fcfe28c973bb6cb2f02a9ad250dc05a8e64b3d6
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-03-31 19:42:10 -0600

    31.03.22
    a

--- APP.py
+++ APP.py
@@ -1,4 +1,6 @@
+from operator import truth
 import streamlit as st
+from sympy import true
 from Paginas.SOFAing import Neu, Resp, cardio, coag, metabol, urin
 import numpy as np
 import pandas as pd
@@ -7,22 +9,55 @@ import matplotlib.pyplot as plt
 import numpy as np
 import Paginas.censo as censo
 #Base de datos
+
 con = sqlite3.connect('/Users/alonso/CxColCardio/otraprueba.db')
 cur = con.cursor()
+menú=st.sidebar.selectbox("Menú",['Censo','Capturar datos','Resultados'])
 st.sidebar.image("/Users/alonso/CxColCardio/Paginas/Imagenes/CMN SXXI.jpeg", None)
-st.sidebar.text_input('Nombre completo')
-st.sidebar.number_input("NSS",1,1111111111)
-st.sidebar.text_input("Agregado abreviado (Ejem 1F89OR)")
-menú=st.sidebar.selectbox("Menú",['Censo','Registro','Resultados'])
-st.subheader(menú)
+
+censo.crear_tabla()
+
+    
 
 #Censo, incluye la tabla de los pacientes del estudio, seleccionar y borrar datos
 if menú=='Censo':
-    base='Basededatos.db'
-    opciones_menu=st.selectbox('Opciones (censo)',['Visualización de censo','Buscar paciente','Registrar paciente'])
-    if opciones_menu=='Visualización de censo':
-        censo.visualizacion ()
-    elif opciones_menu=='Registrar paciente':
-        censo.insertar()
+    st.image('Censo.png',None,200)
+
+    col1,col2=st.columns(2)
+    with col1:
+        with st.expander("Buscar"):
+            con = sqlite3.connect('Basededatos.db')
+            cur = con.cursor()
+            sumedad=cur.execute('''Select Nombre FROM cxcolcardio''')
+            nom=cur.fetchall()
+            nombre=st.selectbox('Nombre',nom)
+            busqueda=st.checkbox('Buscar')
+            if busqueda==True:
+                censo.buscar(nombre)
+    fol1,fol2,fol3=st.columns(3)
+    
+    #Expander para registrar paciente
+    with col2:
+        with st.expander('Registrar paciente'):
+            nombre=st.text_input('Nombre completo')
+            NSS=st.text_input("NSS")
+            edad=st.number_input('Edad',1,120,1,1)
+            prediagnostico=st.multiselect('Diagnóstico',{'CCLA':1,'Colelitiasis':2,'Piocolecisto':3})
+            #no se pueden grabar listas en sql, con repr se hacen strings y se guardan, para separarlos tendremos que utilizar otro codigo posteriormente
+            diagnostico=repr(prediagnostico)
+            genero=st.text_input('Genero')
+            captura=st.checkbox('Capturado')
+            regis_censo=st.button("Registrar en el censo")
+            if regis_censo==True:
+                censo.insertar(nombre,edad,NSS,diagnostico,genero,captura)
+    censo.visualizacion ()
+
+
+        
+        
+    
+
+    
+    
     
     
--- Paginas/censo.py
+++ Paginas/censo.py
@@ -1,31 +1,18 @@
 from mailbox import NoSuchMailboxError
+from tkinter.filedialog import dialogstates
 import streamlit as st
 import sqlite3
 import pandas as pd
 import numpy as np
 
-def insertar():
+def insertar(nombre,edad,NSS,diagnostico,genero,captura):
     con = sqlite3.connect('Basededatos.db')
     cur = con.cursor()
-    
-    col1,col2,col3=st.columns(3)
-    with col1:
-        nombre=st.text_input("Nombre")
-    with col2:
-        edad=st.number_input("Edad",1,120)
-    with col3:
-        nss=st.number_input("NSS",1)
-    with col1:
-        capt=st.checkbox('Capturado')
-        
-    insertar=st.button("Insertar al censo")
-    if insertar==True:
-        try:
-            cur.execute("INSERT INTO cxcolcardio VALUES (?,?,?,?)",(nombre,edad,nss,capt))
-            con.commit()
-            con.close()
-        except:
-            st.subheader("Paciente ya registrado")
+    cur.execute("INSERT INTO cxcolcardio VALUES (?,?,?,?,?,?)",(nombre,edad,NSS,diagnostico,genero,captura))
+    con.commit()
+    con.close()
+    st.subheader("Paciente ya registrado")
+    st.balloons()
     
 def censo(base):
     con = sqlite3.connect('otraprueba.db')
@@ -53,11 +40,11 @@ def capturado():
     x,=edaddf
     st.subheader(x)
     
-def crear_tabla(base):
+def crear_tabla():
     con = sqlite3.connect('Basededatos.db')
     cur = con.cursor()
     cur.execute('''CREATE TABLE IF NOT EXISTS cxcolcardio
-              (Nombre TEXT, Edad INT, NSS INT PRIMARY KEY, Diagnostico TEXT, Genero TEXT, Captura BOOL)''')
+              (Nombre TEXT, Edad INT, NSS TEXT PRIMARY KEY, Diagnostico TEXT, Genero TEXT, Captura TEXT)''')
     con.commit()
     con.close()
     
@@ -65,37 +52,33 @@ def visualizacion():
     con = sqlite3.connect('Basededatos.db')
     cur = con.cursor()
     dxu=pd.read_sql_query("SELECT * from cxcolcardio", con)
-    st.dataframe(dxu)
+    st.table(dxu)
     con.commit()
     con.close
     
-def buscar(id):
-    con = sqlite3.connect('otraprueba.db')
-    cur = con.cursor()
-    dxu=pd.read_sql_query("SELECT NSS from nombre", con)
-    dyu=pd.read_sql_query("SELECT Nombre from nombre", con)
-    col1,col2,col3=st.columns(3)
-    with col1:
-        igu=st.selectbox("NSS",(dxu))
-    with col2:
-        hu=st.selectbox("Nombre",(dyu))
-        Yi=[hu]
-    with col3:
-        st.checkbox("Registrado")
-        sumedad=cur.execute('''Select* FROM nombre WHERE Nombre=(?)''',(Yi))
-        fe=cur.fetchone()
-        st.text_input('Nombre',fe[0])
-        st.number_input('Edad',fe[1])
-        st.subheader(fe)
-        borrar=st.button('Borrad')
-        if borrar== True:
-            sumedad=cur.execute('''DELETE FROM nombre WHERE Nombre=(?)''',(Yi))
-            con.commit()
-            con.close
-
+def buscar(nombre):
+    con = sqlite3.connect('Basededatos.db')
+    cur = con.cursor()   
+    sumedad=cur.execute('''Select *  FROM cxcolcardio WHERE Nombre=(?)''',(nombre))
+    fe=cur.fetchall()
+    st.success('Resultados de búsqueda')
+    st.dataframe(fe)
+    xer=cur.execute('''DELETE FROM cxcolcardio WHERE Captura=No''')
+    con.commit()
+    con.close()
+    borrar=st.button('Borrar')
+    if borrar== True:
+        sumedad=cur.execute('''DELETE FROM cxcolcardio WHERE Nombre=(?)''',(nombre))
+        con.commit()
+        con.close
         
 
-      
+def nombre_base():
+    con = sqlite3.connect('Basededatos.db')
+    cur = con.cursor()
+    sumedad=cur.execute('''Select Nombre FROM cxcolcardio''')
+    edaddf=cur.fetchall()
+    x,=edaddf
+    st.subheader(x) 
 
-    con.commit
-    con.close
+      
